d5be3c779e97462f02b7bd5ddfeb24e4ed03e6ea
Lewis White <16711731@protonmail.com>
2022-05-28 17:49:25 +0100

    Remove frontend bloat, add Bulma and Datatables

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
-require('./bootstrap');
+require( 'jquery' );
+require( 'datatables.net-bm' )();
+require( 'datatables.net-buttons-bm' )();
+require( 'datatables.net-buttons/js/buttons.html5.js' )();
+require( 'datatables.net-responsive-bm' )();
