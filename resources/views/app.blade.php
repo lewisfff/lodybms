8a7be6fdfd8b787cd642c3f964ce2a671eee0c3f
Lewis White <16711731@protonmail.com>
2022-05-29 09:13:26 +0100

    Add a website title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>My custom Bulma website</title>
+    <title>{{ env('APP_NAME') }}</title>
     <link rel="stylesheet" href="/css/app.css">
     <script type="text/javascript" src="https://cdn.datatables.net/v/bm/jq-3.6.0/dt-1.12.1/b-2.2.3/b-html5-2.2.3/r-2.3.0/datatables.min.js"></script>
 </head>
