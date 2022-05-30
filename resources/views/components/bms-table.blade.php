a47476c4267d73d7509bf529aaf60fb47b5df749
Lewis White <16711731@protonmail.com>
2022-05-30 20:03:24 +0100

    Hide useless columns in the table

--- resources/views/components/bms-table.blade.php
+++ resources/views/components/bms-table.blade.php
@@ -5,6 +5,8 @@
     <th>md5</th>
     <th>title</th>
     <th>artist</th>
+    <th>subartist</th>
+    <th>subtitle</th>
     <th>genre</th>
     <th>backbmp</th>
     <th>banner</th>
@@ -26,8 +28,6 @@
     <th>path</th>
     <th>preview</th>
     <th>stagefile</th>
-    <th>subartist</th>
-    <th>subtitle</th>
     <th>tag</th>
     </thead>
     <tbody></tbody>
@@ -48,30 +48,30 @@
                 {data: 'md5', name: 'md5', responsivePriority: 5},
                 {data: 'title', name: 'title', responsivePriority: 0},
                 {data: 'artist', name: 'artist', responsivePriority: 0},
+                {data: 'subartist', name: 'subartist', responsivePriority: 1},
+                {data: 'subtitle', name: 'subtitle', responsivePriority: 1},
                 {data: 'genre', name: 'genre', responsivePriority: 0},
-                {data: 'backbmp', name: 'backbmp', responsivePriority: 1},
-                {data: 'banner', name: 'banner', responsivePriority: 1},
-                {data: 'content', name: 'content', responsivePriority: 1},
-                {data: 'adddate', name: 'adddate', responsivePriority: 1},
-                {data: 'date', name: 'date', responsivePriority: 0},
+                {data: 'backbmp', name: 'backbmp', responsivePriority: 1, visible: false},
+                {data: 'banner', name: 'banner', responsivePriority: 1, visible: false},
+                {data: 'content', name: 'content', responsivePriority: 1, visible: false},
+                {data: 'adddate', name: 'adddate', responsivePriority: 1, visible: false},
+                {data: 'date', name: 'date', responsivePriority: 0, visible: false,},
                 {data: 'difficulty', name: 'difficulty', responsivePriority: 0},
-                {data: 'favorite', name: 'favorite', responsivePriority: 0},
+                {data: 'favorite', name: 'favorite', responsivePriority: 0, visible: false},
                 {data: 'feature', name: 'feature', responsivePriority: 1},
-                {data: 'folder', name: 'folder', responsivePriority: 1},
+                {data: 'folder', name: 'folder', responsivePriority: 1, visible: false},
                 {data: 'judge', name: 'judge', responsivePriority: 1},
                 {data: 'length', name: 'length', responsivePriority: 0},
-                {data: 'level', name: 'level', responsivePriority: 0},
+                {data: 'level', name: 'level', responsivePriority: 0, visible: false},
                 {data: 'maxbpm', name: 'maxbpm', responsivePriority: 0},
                 {data: 'minbpm', name: 'minbpm', responsivePriority: 0},
                 {data: 'mode', name: 'mode', responsivePriority: 1},
                 {data: 'notes', name: 'notes', responsivePriority: 0},
-                {data: 'parent', name: 'parent'},
+                {data: 'parent', name: 'parent', responsivePriority: 1, visible: false},
                 {data: 'path', name: 'path', responsivePriority: 1},
-                {data: 'preview', name: 'preview', responsivePriority: 1},
-                {data: 'stagefile', name: 'stagefile', responsivePriority: 1},
-                {data: 'subartist', name: 'subartist', responsivePriority: 1},
-                {data: 'subtitle', name: 'subtitle', responsivePriority: 1},
-                {data: 'tag', name: 'tag', responsivePriority: 1},
+                {data: 'preview', name: 'preview', responsivePriority: 1, visible: false},
+                {data: 'stagefile', name: 'stagefile', responsivePriority: 1, visible: false},
+                {data: 'tag', name: 'tag', responsivePriority: 1, visible: false},
             ]
         });
 
